f4598041e8008f3d3225ea83e9b7a17e069b9881
Taguilar6 <taguilar6@ivytech.edu>
2025-04-27 21:18:29 -0400

    Add files via upload

---
+++ My first Python Program Module 6 assignment - Tatiana Aguilar raining cats and dogs.py
@@ -0,0 +1,12 @@
+# Set c to "cats"
+c = "cats"
+
+# Set d to "dogs"
+d = "dogs"
+
+# Set s to the sentence needed to print
+# Writing the sentence using c and d
+s = "It is raining " + c + " and " + d + "."
+
+# Print the final sentence
+print(s)
